7f63a602b97c09d781d9207b0f899ee382547220
Kevin907 <kevin22370243@gmail.com>
2023-01-13 16:35:49 +0800

    no change

---
+++ control.v
@@ -0,0 +1,5 @@
+module board_move(
+    input wire [9:0] board_x
+);
+
+endmodule
--- main.v
+++ main.v
@@ -36,6 +36,34 @@ module main(
 
     clock_divider #(.n(22)) clock_divider_22(.clk(clk), .rst(rst_pb), .clk_div(clk_22));
 
+    reg [3:0] key_num;
+    wire [511:0] key_down;
+    wire [8:0] last_change;
+    wire been_ready;
+
+    parameter keyA = 9'b0_0001_1100;
+    parameter keyD = 9'b0_0010_0011;
+
+    KeyboardDecoder key_de (
+        .key_down(key_down),
+        .last_change(last_change),
+        .key_valid(been_ready),
+        .PS2_DATA(PS2_DATA),
+        .PS2_CLK(PS2_CLK),
+        .rst(rst_pb),
+        .clk(clk)
+    );
+
+    always @(*) begin
+        next_board_x = board_x;
+        if(been_ready && key_down[last_change] == 1'b1) begin
+            if(key_down[keyA]) next_board_x = (board_x < 370) ? board_x + 5 : board_x; // A
+            else if(key_down[keyD]) next_board_x = (board_x > 100) ? board_x - 5 : board_x; // D
+        end
+    end
+
+
+
     always @(posedge clk_22, posedge rst) begin
         if(rst) begin
             bricks <= 1440'd0;
@@ -44,6 +72,7 @@ module main(
             ball_vx <= 10'd8;
             ball_vy <= 10'd6;
             ball_dir <= 2'b10; // right/up
+            board_x <= 272;
         end
         else begin
             ball_x <= next_ball_x;
